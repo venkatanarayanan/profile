e64a87db5769ffba441e8fa2506bde0e11357ccb
unknown <venkatanarayanan22@gmail.com>
2021-06-24 00:19:23 +0530

    Added layout for skills section along with Radar

--- app.R
+++ app.R
@@ -61,6 +61,36 @@ ui <- navbarPage(
              column(12,
                     div(style = "font-weight: bold;
                       font-size:38px;
+                      text-align:center;margin: 50px 2px 50px 0px;
+                      color:mediumseagreen;",
+                        "Skills",
+                    ),
+             )
+           ),
+           fluidRow(
+             style = "font-size:12px;
+                      margin: 20px 2px 20px 0px;
+                      color:#ffffff;
+             box-shadow: 8px 4px 8px 4px rgba(0, 0, 0, 0.2), 20px 6px 20px 6px rgba(0, 0, 0, 0.19);",
+             column(6,
+                    img(src = "skill.png",
+                        width = "500",
+                        height = "500")
+             ),
+             column(6,
+                    div(
+                      style = "font-size:18px;
+                      text-align:left;
+                      margin: 20px;
+                      color:#ffffff;",
+                      textOutput("skillsContent")
+                    )
+             )
+           ),
+           fluidRow(
+             column(12,
+                    div(style = "font-weight: bold;
+                      font-size:38px;
                       text-align:center;margin: 20px 2px 20px 0px;
                       color:mediumseagreen;",
                         "Social Profiles",
@@ -133,6 +163,15 @@ server <- function(input, output, session) {
   }) 
   
   output$otherInterests <- renderText({
+    
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+    It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+    
+  })
+  
+  output$skillsContent <- renderText({
     
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
